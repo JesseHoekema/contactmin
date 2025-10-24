752482f95c5132f67921d6e9d81514678b07b29f
Jesse Hoekema <109305882+JesseHoekema@users.noreply.github.com>
2025-10-24 14:58:46 +0200

    fix idk???

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,14 @@
-import pkg from "@prisma/client";
+import pkg from '@prisma/client';
 const { PrismaClient } = pkg;
 
-export const prisma = new PrismaClient();
+declare global {
+  var prisma: InstanceType<typeof PrismaClient> | undefined;
+}
+
+const prisma = globalThis.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') {
+  globalThis.prisma = prisma;
+}
+
+export { prisma };
